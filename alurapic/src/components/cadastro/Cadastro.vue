91ce67a77c4e527782688e240710c743824bc57b
Daniel Boso <bosodaniel@hotmail.com>
2021-01-21 11:14:19 -0300

    finish

--- alurapic/src/components/cadastro/Cadastro.vue
+++ alurapic/src/components/cadastro/Cadastro.vue
@@ -1,13 +1,120 @@
+<!-- alurapic/src/components/cadastro/Cadastro.vue -->
+
 <template>
-    <h2 class="centralizado">Cadastro</h2>
+
+  <div>
+    <h1 class="centralizado">Cadastro</h1>
+    <h2 class="centralizado">{{ foto.titulo }}</h2>
+
+    <h2 v-if="foto._id" class="centralizado">Alterando</h2>
+    <h2 v-else class="centralizado">Incluindo</h2>
+
+    <form @submit.prevent="grava()">
+      <div class="controle">
+        <label for="titulo">TÍTULO</label>
+        <input data-vv-as="título" v-validate data-vv-rules="required|min:3|max:30" name="titulo" id="titulo" autocomplete="off" v-model="foto.titulo">
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('titulo') }}</span>
+      </div>
+
+      <div class="controle">
+        <label for="url">URL</label>
+        <input v-validate data-vv-rules="required" name="url" id="url" autocomplete="off" v-model="foto.url">
+        <imagem-responsiva v-show="foto.url" :url="foto.url" :titulo="foto.titulo"/>
+        <span class="erro" v-show="errors.has('titulo')">{{ errors.first('url') }}</span>
+      </div>
+
+      <div class="controle">
+        <label for="descricao">DESCRIÇÃO</label>
+        <textarea id="descricao" autocomplete="off" v-model="foto.descricao"></textarea>
+      </div>
+
+      <div class="centralizado">
+        <meu-botao rotulo="GRAVAR" tipo="submit"/>
+        <router-link :to="{ name: 'home' }"><meu-botao rotulo="VOLTAR" tipo="button"/></router-link>
+      </div>
+
+    </form>
+  </div>
 </template>
+
 <script>
 
-</script>
-<style>
+import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue'
+import Botao from '../shared/botao/Botao.vue';
+import Foto from '../../domain/foto/Foto';
+import FotoService from '../../domain/foto/FotoService';
 
-    .centralizado {
+export default {
 
-        text-align: center;
+  components: {
+
+    'imagem-responsiva': ImagemResponsiva, 
+    'meu-botao': Botao
+  }, 
+
+  data() {
+    return {
+        foto: new Foto(),
+        id: this.$route.params.id
     }
+  },
+
+  created() {
+    this.service = new FotoService(this.$resource);
+
+    if(this.id) {
+      this.service.busca(this.id).then(foto => this.foto = foto)
+    }
+  },
+
+  methods: {
+
+    grava() {
+
+      this.$validator.validateAll().then(success => {
+
+        if(success) {
+          this.service.cadastra(this.foto)
+            .then(() => {
+              if(this.id) {
+                this.$router.push({ name: 'home' })
+              }
+              this.foto = new Foto()
+            }, err => console.log(err));
+        }
+      });
+    }
+  }
+}
+
+</script>
+<style scoped>
+
+  .centralizado {
+    text-align: center;
+  }
+  .controle {
+    font-size: 1.2em;
+    margin-bottom: 20px;
+
+  }
+  .controle label {
+    display: block;
+    font-weight: bold;
+  }
+
+ .controle label + input, .controle textarea {
+    width: 100%;
+    font-size: inherit;
+    border-radius: 5px
+  }
+
+  .centralizado {
+    text-align: center;
+  }
+
+  .erro {
+    color: red;
+  }
+
 </style>
--- alurapic/src/components/home/Home.vue
+++ alurapic/src/components/home/Home.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
+    <img src="/static/image.png" height="70px;">
     <h1 class="centralizado">{{ titulo }}</h1>
+    <p v-show="mensagem" class="centralizado">{{ mensagem }}</p>
 
     <input type="search" class="filtro" @input="filtro = $event.target.value" placeholder="filtre por parte do título">
 
@@ -8,9 +10,12 @@
       <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
 
         <meu-painel :titulo="foto.titulo">
-            <imagem-responsiva :url="foto.url" :titulo="foto.titulo"/>
-            <meu-botao tipo="button" rotulo="REMOVER" 
-              @botaoAtivado="remove(foto)" :confirmacao="false" estilo="perigo"/>
+          <imagem-responsiva v-meu-transform:scale.animate="1.1" :url="foto.url" :titulo="foto.titulo"/>
+          
+          <router-link :to="{ name: 'altera', params: { id: foto._id } }">
+            <meu-botao tipo="button" rotulo="ALTERAR" @botaoAtivado="altera(foto)"/>
+          </router-link>
+          <meu-botao tipo="button" rotulo="REMOVER" @botaoAtivado="remove(foto)" :confirmacao="true" estilo="perigo"/>
         </meu-painel>
 
       </li>
@@ -21,7 +26,8 @@
 <script>
 import Painel from '../shared/painel/Painel.vue';
 import ImagemResponsiva from '../shared/imagem-responsiva/ImagemResponsiva.vue';
-import Botao from '../shared/botao/botao.vue';
+import Botao from '../shared/botao/Botao.vue';
+import FotoService from '../../domain/foto/FotoService'; 
 
 export default {
 
@@ -37,7 +43,8 @@ export default {
 
       titulo: 'Alurapic', 
       fotos: [], 
-      filtro: ''
+      filtro: '',
+      mensagem: ''
     }
   },
 
@@ -56,15 +63,20 @@ export default {
 
   methods: {
     remove(foto) {
-      alert('Remover a foto ' + foto.titulo);
+
+      this.service.apaga(foto._id)
+        .then(() => {
+            let indice = this.fotos.indexOf(foto);
+            this.fotos.splice(indice, 1)
+            this.mensagem = "Foto removida com sucesso";
+        }, err => this.mensagem = err.message);
     }
   },
 
   created() {
 
-    this.$http.get('http://localhost:3000/v1/fotos')
-      .then(res => res.json())
-      .then(fotos => this.fotos = fotos, err => console.log(err));
+    this.service = new FotoService(this.$resource);
+    this.service.lista().then(fotos => this.fotos = fotos, err => this.mensagem = err.message);
   }
 }
 
